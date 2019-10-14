0ec3f7635a3dcf49fffc14c461dce35d8de81af4
Die <48467821+RohanPunjani@users.noreply.github.com>
2019-10-14 12:01:50 +0530

    Update add_pat.php

--- hms/assets/pages/add_pat.php
+++ hms/assets/pages/add_pat.php
@@ -8,14 +8,14 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Adding Doctor</title>
+    <title>Adding Patient</title>
 </head>
 <body>
     <div class="jumbotron">
         <h1 align="center">Add a new Patient</h1>
     </div>
     <div class="container">
-    <form action="insert_doc.php" method="POST">
+    <form action="insert_pat.php" method="POST">
         <div class="form-group">
             <label for="name">Name: </label>
             <input type="text" class="form-control" id="name" name="pat_name">
